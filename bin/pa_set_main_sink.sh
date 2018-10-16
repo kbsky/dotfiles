1d3b260f2fe5456bd60e8475e1071495b9f72ccb
Kevin Brodsky <corax26@gmail.com>
2018-10-16 19:09:24 +0100

    pa_set_main_sink: print sink name on success

--- bin/pa_set_main_sink.sh
+++ bin/pa_set_main_sink.sh
@@ -9,3 +9,6 @@ pactl set-default-sink "$sink"
 for input in $(pactl list sink-inputs short | cut -d$'\t' -f1); do
     pactl move-sink-input "$input" "$sink"
 done
+
+pretty_name=$(pactl list sinks | perl -n0E "/Name: $sink\n\s*Description: (.*)/ && print \$1")
+notify-send -i audio-card -h int:transient:1 -u low "Default sink set to $pretty_name"
