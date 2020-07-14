a272d033c32d75e3b6bea532d10c3896597700c7
Kevin Brodsky <corax26@gmail.com>
2020-07-14 23:43:36 +0100

    vim: add TagHL as a submodule
    Now that there is a mirror in GitHub, we can add it as a submodule
    and get rid of init-config.sh.

--- bin/init-config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# General
-cd
-echo "## Init and update submodules"
-git submodule update --init --recursive
-
-# Tag-Highlight
-echo "## Clone Tag-Highlight"
-hg clone https://bitbucket.org/abudden/taghighlight .vim/bundle/tag-highlight
