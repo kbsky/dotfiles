25f8ec6b32c383bd4acb1773fc82884950ecc692
Kevin Brodsky <corax26@gmail.com>
2021-03-12 19:49:52 +0000

    pa_show_default_source_muted: use pactl instead of pacmd
    pactl is part of libpulse, whereas pacmd is part of pulseaudio, and
    therefore not available when using pipewire.

--- bin/pa_show_default_source_muted.sh
+++ bin/pa_show_default_source_muted.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-pacmd list-sources | awk '/^\s*\* index/,/^\s*index/ { if (/muted:/) { print ($2 == "yes" ? "Muted" : "Listening"); exit } }'
+cur_src=$(pactl info | grep "Default Source" | cut -f3 -d" ")
+pactl list sources | awk '/Name: '"$cur_src"'$/,/^Source/ { if (/Mute:/) { print ($2 == "yes" ? "Muted" : "Listening"); exit } }'
