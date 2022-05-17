1b6cacd9c1d03ff7dd8a81ab4f953f08beef51a0
Kevin Brodsky <corax26@gmail.com>
2022-05-17 21:51:01 +0200

    update-mirrorlist: new home :)

--- bin/update-mirrorlist.sh
+++ bin/update-mirrorlist.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -x
-sudo reflector --verbose --threads 8 -n 20 -c 'United Kingdom' -p https --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --verbose --threads 8 -n 20 -c 'Sweden' -p https --sort rate --save /etc/pacman.d/mirrorlist
 { set +x; } 2>/dev/null
